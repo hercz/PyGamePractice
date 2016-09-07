97146a75a1812898ee4065dc762951144cf5b5e9
Herczku Misi <kmisku@gmail.com>
2016-09-07 14:25:55 +0200

    Add movements for keypress

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,7 +6,6 @@ windowSize = (800, 600)
 
 screen = pygame.display.set_mode(windowSize)
 
-
 helloWorld = pygame.image.load("mario-icon.png")
 helloWorldSize = helloWorld.get_size()
 
@@ -24,27 +23,37 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            x += 5
+        if event.key == pygame.K_LEFT:
+            x -= 5
+
+        if event.key == pygame.K_DOWN:
+            y += 5
+        if event.key == pygame.K_UP:
+            y -= 5
+
     screen.fill((0, 0, 0))
 
-
-    mousePosition = pygame.mouse.get_pos()
-
-    x, y = mousePosition
-
-    if x + helloWorldSize[0] > 800 :
+    if x + helloWorldSize[0] > 800:
         x = 800 - helloWorldSize[0]
         sound.stop()
         sound.play()
 
-    if y + helloWorldSize[1] >600:
+    if y + helloWorldSize[1] > 600:
         y = 600 - helloWorldSize[1]
         sound.stop()
         sound.play()
 
-    if x == 0 or y == 0:
+    if x <= 0:
         sound.stop()
         sound.play()
-
+        x = 0
+    if y <= 0:
+        sound.stop()
+        sound.play()
+        y = 0
 
     screen.blit(helloWorld, (x, y))
 
